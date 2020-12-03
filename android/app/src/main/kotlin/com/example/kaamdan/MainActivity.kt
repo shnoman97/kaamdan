e7444a8a17bd1d462a730c912446c916443be600
shnoman97 <nomisheikh0@gmail.com>
2020-12-03 21:16:58 +0500

    Login UI

---
+++ android/app/src/main/kotlin/com/example/kaamdan/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kaamdan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/routes.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/widgets.dart';
+import 'package:kaamdan/screens/complete_profile/complete_profile_screen.dart';
+
+import 'package:kaamdan/screens/forgot_password/forgot_password_screen.dart';
+import 'package:kaamdan/screens/login_success/login_success_screen.dart';
+import 'package:kaamdan/screens/otp/otp_screen.dart';
+import 'package:kaamdan/screens/sign_in/sign_in_screen.dart';
+import 'package:kaamdan/screens/sign_up/sign_up_screen.dart';
+import 'package:kaamdan/screens/splash/splash_screen.dart';
+
+
+
+// We use name route
+// All our routes will be available here
+final Map<String, WidgetBuilder> routes = {
+  SplashScreen.routeName: (context) => SplashScreen(),
+  SignInScreen.routeName: (context) => SignInScreen(),
+  ForgotPasswordScreen.routeName: (context) => ForgotPasswordScreen(),
+  LoginSuccessScreen.routeName: (context) => LoginSuccessScreen(),
+  SignUpScreen.routeName: (context) => SignUpScreen(),
+  CompleteProfileScreen.routeName: (context) => CompleteProfileScreen(),
+  OtpScreen.routeName: (context) => OtpScreen(),
+};
